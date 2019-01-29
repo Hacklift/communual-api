510d5d5bc92c189765b688e674439d7c7365d6e4
valentineezeh <val.ezeh15@gmail.com>
2019-01-29 15:56:50 +0100

    IIMPLement user sign up feature - Set up and configure database - Configure environmental variables - Implement table creation by configuring migrations - Set up and configure base app - Implement sign up controller - Implement user input validations - Write tests

---
+++ routes/routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask_restful import Api
+from controllers.auth_controllers.Sign_up import RegistrationView
+
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# Routes
+api.add_resource(RegistrationView, '/auth/signup')
